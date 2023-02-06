bb58b92868404b4b9ea9d55ff7180e26219be1f9
luigina <l.costante1@studenti.unisa.it>
2023-02-06 17:05:53 +0100

    modifiche varie e testing classe QuizDAO

--- app/src/main/java/com/example/irepeat/MainActivity.java
+++ app/src/main/java/com/example/irepeat/MainActivity.java
@@ -6,8 +6,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.irepeat.Bean.QuizBean;
 import com.example.irepeat.Bean.UtenteBean;
 import com.example.irepeat.DAO.DBHelper;
+import com.example.irepeat.DAO.QuizDAO;
 import com.example.irepeat.DAO.UtenteDAO;
 
 import java.sql.Connection;
@@ -20,27 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
-
-        /*Connection conn= null;
-        try {
-            conn= DriverManager.getConnection("jdbc:sqlite:iRepeat.db");
-            Statement statement= conn.createStatement();
-            statement.executeUpdate("DROP DATABASE iRepeat");
-            Log.d("MYDEBUG","DB CANCELLATO");
-        } catch (SQLException e) {
-            Log.d("MYDEBUG","eccezione 1");
-        }
-        finally {
-            if(conn!=null){
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    Log.d("MYDEBUG","eccezione 2");
-                }
-            }
-        }*/
 
 
         DBHelper db= new DBHelper(this);
@@ -49,17 +33,26 @@ public class MainActivity extends AppCompatActivity {
         //db.deleteDatabase(this);
         //db= new DBHelper(this);
         //db.onCreate(db.getWritableDatabase());
-        UtenteDAO utenteDAO= new UtenteDAO(db);
+        /*UtenteDAO utenteDAO= new UtenteDAO(db);
         utenteDAO.open();
         UtenteBean utente= new UtenteBean("luigina@iRepeat.it", "luigina01", "prova bio", "luigina", "costante", "luigina01");
-        //utenteDAO.insert(utente);
+        utenteDAO.insert(utente);
 
         utenteDAO.update(utente);
 
-        //Log.d("MYDEBUG", utente.toString());
+        Log.d("MYDEBUG", utente.toString());
         Log.d("MYDEBUG", String.valueOf(utenteDAO.select(utente.getEmail(), utente.getPassword())));
 
-        utenteDAO.close();
+        utenteDAO.close();*/
+
+        QuizDAO quizDAO = new QuizDAO(db);
+        quizDAO.open();
+        UtenteBean utente= new UtenteBean("luigina@iRepeat.it", "luigina01", "prova bio", "luigina", "costante", "luigina01");
+        QuizBean quiz= new QuizBean("descrizione", "nome", "disciplina", 1, "durata", 0, utente);
+
+        quizDAO.insert(quiz);
+
+        Log.d("MYDEBUG", String.valueOf(quizDAO.select(quiz.getId())));
 
     }
 }
