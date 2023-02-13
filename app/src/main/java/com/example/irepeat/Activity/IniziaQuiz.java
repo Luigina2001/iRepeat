e4b7cf674056187128f5d0e4cbe9352d38374a5a
luigina <l.costante1@studenti.unisa.it>
2023-02-13 17:50:49 +0100

    modifiche varie per IniziaQuiz

--- app/src/main/java/com/example/irepeat/Activity/IniziaQuiz.java
+++ app/src/main/java/com/example/irepeat/Activity/IniziaQuiz.java
@@ -1,5 +1,6 @@
 package com.example.irepeat.Activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -8,8 +9,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.irepeat.Adapter.OpzioniRispostaAdapter;
@@ -27,6 +30,7 @@ public class IniziaQuiz extends AppCompatActivity {
 
     private ArrayList<DomandaBean> domande;
     private HashMap<DomandaBean, ArrayList<RispostaBean>> risposte= new HashMap<>();
+    private HashMap<DomandaBean, RispostaBean> risposteDate= new HashMap<>();
     private TextView numeroDomanda, testoDomanda, numeroDomandaOmbra;
     private int count=0;
     private OpzioniRispostaAdapter adapter;
@@ -66,11 +70,11 @@ public class IniziaQuiz extends AppCompatActivity {
         numeroDomandaOmbra.setText("Domanda n."+(count+1));
 
         testoDomanda= findViewById(R.id.testoDomanda);
-        testoDomanda.setText(domande.get(0).getTesto());
+        testoDomanda.setText(domande.get(count).getTesto());
 
         listView= findViewById(R.id.opzioniRisposte);
-        adapter = new OpzioniRispostaAdapter(this, R.layout.list_element_opzioni_risposta, risposte.get(domande.get(0)));
-        for (RispostaBean r: risposte.get(domande.get(0))){
+        adapter = new OpzioniRispostaAdapter(this, R.layout.list_element_opzioni_risposta, risposte.get(domande.get(count)), this);
+        for (RispostaBean r: risposte.get(domande.get(count))){
             Log.d("MYDEBUG_INIZIA_QUIZ", r.getTesto());
         }
         listView.setAdapter(adapter);
@@ -78,21 +82,95 @@ public class IniziaQuiz extends AppCompatActivity {
     }
 
     public void onClickTerminaQuiz (View v){
-        //messaggio di conferma??
 
-        Intent i= new Intent(this, EsitoQuiz.class);
-        startActivity(i);
+        new AlertDialog.Builder(this)
+                .setMessage("Sei sicuro di voler terminare il quiz?")
+                .setCancelable(false)
+                .setPositiveButton("Sì", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i= new Intent(getApplicationContext(), EsitoQuiz.class);
+                        startActivity(i);
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     public void onClickPrecedente (View v){
+        if (--count >=0) {
+            numeroDomanda.setText("Domanda n." + (count + 1));
+            numeroDomandaOmbra.setText("Domanda n." + (count + 1));
 
+            testoDomanda = findViewById(R.id.testoDomanda);
+            testoDomanda.setText(domande.get(count).getTesto());
+
+            listView = findViewById(R.id.opzioniRisposte);
+            adapter = new OpzioniRispostaAdapter(this, R.layout.list_element_opzioni_risposta, risposte.get(domande.get(count)), this);
+            for (RispostaBean r : risposte.get(domande.get(count))) {
+                Log.d("MYDEBUG_INIZIA_QUIZ", r.getTesto());
+            }
+            listView.setAdapter(adapter);
+            if (risposteDate.get(domande.get(count))!=null){
+                int i=0;
+                for (RispostaBean r: risposte.get(domande.get(count))){
+                    if (r.equals(risposteDate.get(domande.get(count))))
+                        break;
+                    i++;
+                }
+                adapter.setSelectedPosition(i);
+            }
+        }
+        else{
+            count++;
+            Toast.makeText(this, "Questa è la prima domanda", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onClickSuccessiva (View v){
+        if (++count < domande.size()) {
+
+            numeroDomanda.setText("Domanda n." + (count + 1));
+            numeroDomandaOmbra.setText("Domanda n." + (count + 1));
+
+            testoDomanda = findViewById(R.id.testoDomanda);
+            testoDomanda.setText(domande.get(count).getTesto());
+
+            listView = findViewById(R.id.opzioniRisposte);
+            adapter = new OpzioniRispostaAdapter(this, R.layout.list_element_opzioni_risposta, risposte.get(domande.get(count)), this);
+            for (RispostaBean r : risposte.get(domande.get(count))) {
+                Log.d("MYDEBUG_INIZIA_QUIZ", r.getTesto());
+            }
+            listView.setAdapter(adapter);
+            if (risposteDate.get(domande.get(count))!=null){
+                int i=0;
+                for (RispostaBean r: risposte.get(domande.get(count))){
+                    if (r.equals(risposteDate.get(domande.get(count))))
+                        break;
+                    i++;
+                }
+                adapter.setSelectedPosition(i);
+            }
+        }
+        else{
+            count--;
+            Toast.makeText(this, "Questa è l'ultima domanda", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    public void salvaRisposta(int selected){
+        if (selected<0){
+            risposteDate.put(domande.get(count), null);
+        }
+        else {
+            ArrayList<RispostaBean> risposteDomanda= risposte.get(domande.get(count));
+            RispostaBean rispostaData= risposteDomanda.get(selected);
+            risposteDate.put(domande.get(count), rispostaData);
+        }
     }
 }
--- app/src/main/java/com/example/irepeat/Adapter/OpzioniRispostaAdapter.java
+++ app/src/main/java/com/example/irepeat/Adapter/OpzioniRispostaAdapter.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.example.irepeat.Activity.IniziaQuiz;
 import com.example.irepeat.Bean.QuizBean;
 import com.example.irepeat.Bean.RispostaBean;
 import com.example.irepeat.R;
@@ -24,11 +25,14 @@ public class OpzioniRispostaAdapter extends ArrayAdapter<RispostaBean> {
     private LayoutInflater inflater;
     private Context context;
     private int selectedPosition=-1;
+    private IniziaQuiz iniziaQuiz;
 
-    public OpzioniRispostaAdapter(@NonNull Context context, int resource, @NonNull List<RispostaBean> objects) {
+    public OpzioniRispostaAdapter(@NonNull Context context, int resource, @NonNull List<RispostaBean> objects, IniziaQuiz
+            iniziaQuizActivity) {
         super(context, resource, objects);
         inflater = LayoutInflater.from(context);
         this.context=context;
+        this.iniziaQuiz= iniziaQuizActivity;
     }
 
     @Override
@@ -57,6 +61,7 @@ public class OpzioniRispostaAdapter extends ArrayAdapter<RispostaBean> {
             public void onClick(View v) {
                 selectedPosition = position;
                 notifyDataSetChanged();
+                iniziaQuiz.salvaRisposta(position);
             }
         });
 
@@ -66,6 +71,10 @@ public class OpzioniRispostaAdapter extends ArrayAdapter<RispostaBean> {
     public void setSelectedPosition(int selectedPosition) {
         this.selectedPosition = selectedPosition;
         notifyDataSetChanged();
+    }
+
+    public int getSelectedPosition() {
+        return selectedPosition;
     }
 
 
